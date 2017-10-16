4569f7fbc35cf754abae69ebb0cd7320ea693545
Dan Dooley <acehawkbell@gmail.com>
2017-10-17 09:55:15 +1300

    First Run

--- sit_build.sh
+++ sit_build.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Triggering SIT Build"
+
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_API_TOKEN" \
+   -d "$body" \
+   https://api.travis-ci.com/GeoNet/sit/requests
