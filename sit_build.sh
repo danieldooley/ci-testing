df528a66e6ffa723f0dac6a778a8b9720e5f71ab
Dan Dooley <acehawkbell@gmail.com>
2017-10-17 10:23:37 +1300

    Side1

--- sit_build.sh
+++ sit_build.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Triggering SIT Build"
+if ['$TRAVIS_BRANCH' == 'master']
+then
+    echo 'Triggering SIT Build'
 
-export
+    body='{
+    "request": {
+    "branch":"master"
+    }}'
 
-body='{
-"request": {
-"branch":"master"
-}}'
-
-curl -s -X POST \
-   -H "Content-Type: application/json" \
-   -H "Accept: application/json" \
-   -H "Travis-API-Version: 3" \
-   -H "Authorization: token $TRAVIS_API_TOKEN" \
-   -d "$body" \
-   https://api.travis-ci.com/repo/GeoNet%2Fsit/requests
+    curl -s -X POST \
+       -H "Content-Type: application/json" \
+       -H "Accept: application/json" \
+       -H "Travis-API-Version: 3" \
+       -H "Authorization: token $TRAVIS_API_TOKEN" \
+       -d "$body" \
+       https://api.travis-ci.com/repo/GeoNet%2Fsit/requests
+fi
